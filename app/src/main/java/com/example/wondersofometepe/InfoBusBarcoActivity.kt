be14bfd929fffc2c9c073ba84b272ad53c930ffb
Denis <denisnica.242004@gmail.com>
2022-05-16 16:38:29 -0600

    nuevo proyecto de la aplicacion Discover The Wonders Of Ometepe. Diseños casi terminados, iconos incluidos

---
+++ app/src/main/java/com/example/wondersofometepe/InfoBusBarcoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.wondersofometepe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InfoBusBarcoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_bus_barco)
+    }
+}

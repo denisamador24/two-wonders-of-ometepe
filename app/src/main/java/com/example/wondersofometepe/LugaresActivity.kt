7ef47170e8f516508a9d28d21a50bdb16b674c47
Denis <denisnica.242004@gmail.com>
2022-05-18 12:11:17 -0600

    motorios cambios a los estilos y algunas activity. Activity Lugares y Transporte creada

---
+++ app/src/main/java/com/example/wondersofometepe/LugaresActivity.kt
@@ -0,0 +1,11 @@
+package com.example.wondersofometepe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LugaresActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lugares)
+    }
+}
--- app/src/main/java/com/example/wondersofometepe/MainActivity.kt
+++ app/src/main/java/com/example/wondersofometepe/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.wondersofometepe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    val homeFragment : HomeFragment = HomeFragment()
+    val profileFragment : ProfileFragment = ProfileFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainFragment = findViewById<FrameLayout>(R.id.nav_host_fragment)
+        val buttonNavView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        var x = 2
+        var y =8
+        var r = x + y
+
+        setTitle("Hola")
     }
 }

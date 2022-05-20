ee48f4253c95ee07d993e8c560eda6c4cace693d
Denis <denisnica.242004@gmail.com>
2022-05-20 11:54:30 -0600

    cambio de tamanio a imagenes y frgament creads: ListLocals y ListTransport.

--- app/src/main/java/com/example/wondersofometepe/MainActivity.kt
+++ app/src/main/java/com/example/wondersofometepe/MainActivity.kt
@@ -3,26 +3,24 @@ package com.example.wondersofometepe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    val homeFragment : HomeFragment = HomeFragment()
-    val profileFragment : ProfileFragment = ProfileFragment()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mainFragment = findViewById<FrameLayout>(R.id.nav_host_fragment)
+        val navcontroller = findNavController(R.id.nav_host_fragment)
         val buttonNavView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.profileFragment))
+        setupActionBarWithNavController(navcontroller, appBarConfiguration)
 
-        var x = 2
-        var y =8
-        var r = x + y
-
-        setTitle("Hola")
+        buttonNavView.setupWithNavController(navcontroller)
     }
 }
